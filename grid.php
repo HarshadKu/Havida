1dd6289937c6c1456b524546ca1fccda50796115
HarshadKu <121249733+HarshadKu@users.noreply.github.com>
2022-12-22 20:41:24 +0400

    Add files via upload

---
+++ grid.php
@@ -0,0 +1,93 @@
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="Style.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="style/style1.css">
+    
+    </head>
+    <body>
+        <div class="grid-container">
+            <div class="grid-item grid-item-1">
+            <div class="card">
+                 <div class="images"> 
+                    <img src="Images/4.jpg" alt=" image4">
+                 </div>
+                 <div class="Details">
+                    <p>Multicolor Digital Print sarees</p>
+                 </div>
+                 <div class="price">
+                    <p> R = 2500</p>
+                 </div>
+            </div>
+            </div>
+            <div class="grid-item grid-item-2">
+            <div class="card">
+                 <div class="images"> 
+                    <img src="Images/5.jpg" alt=" image5">
+                 </div>
+                 <div class="Details">
+                    <p>Multicolor Digital Print sarees</p>
+                 </div>
+                 <div class="price">
+                    <p> R = 2500</p>
+                 </div>
+            </div>
+            </div>
+            <div class="grid-item grid-item-3">
+            <div class="card">
+                 <div class="images"> 
+                    <img src="Images/6.jpg" alt=" image6">
+                 </div>
+                 <div class="Details">
+                    <p>Multicolor Digital Print sarees</p>
+                 </div>
+                 <div class="price">
+                    <p> R = 2500</p>
+                 </div>
+            </div>
+            </div>
+            <div class="grid-item grid-item-4">
+            <div class="card">
+                 <div class="images"> 
+                    <img src="Images/1.jpg" alt=" image1">
+                 </div>
+                 <div class="Details">
+                    <p>Multicolor Digital Print sarees</p>
+                 </div>
+                 <div class="price">
+                    <p> R = 2500</p>
+                 </div>
+            </div>
+            </div>
+            <div class="grid-item grid-item-5">
+            <div class="card">
+                 <div class="images"> 
+                    <img src="Images/2.jpg" alt=" image2">
+                 </div>
+                 <div class="Details">
+                    <p>Multicolor Digital Print sarees</p>
+                 </div>
+                 <div class="price">
+                    <p> R = 2500</p>
+                 </div>
+            </div>
+            </div>
+            <div class="grid-item grid-item-6">
+            <div class="card">
+                 <div class="images"> 
+                    <img src="Images/4.jpg" alt=" image4">
+                 </div>
+                 <div class="Details">
+                    <a>Multicolor Digital Print sarees</a>
+                 </div>
+                 <div class="price">
+                    <p> R = 2500</p>
+                 </div>
+            </div>
+            </div>
+        </div>
+    </body>
+</html>
